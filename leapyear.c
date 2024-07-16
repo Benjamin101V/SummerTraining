065d60eb10d06e960197515d38a2ff24751530a7
Benjamin101V <benjamin_c@qq.com>
2024-07-16 21:40:55 +0800

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int y;
+    scanf("%d",&y);
+    if(y%4==0 && y%100!=0)
+    
+        printf("Leap year");
+    
+        else if(y%400==0)
+    
+                printf("Leap year");
+    
+    else
+        printf("Not a leap year");
+    
+     return 0;
+}   
