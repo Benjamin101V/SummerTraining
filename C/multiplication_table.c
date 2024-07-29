0eb4ed49e9590fdf37d6b23a4dd3ac7a59c5c020
Benjamin101V <benjamin_c@qq.com>
2024-07-29 21:52:53 +0800

    Update multiplication_table.c

--- C/multiplication_table.c
+++ C/multiplication_table.c
@@ -3,7 +3,8 @@ int main()
 {
     int a,b;
     for(a=1;a<=9;a++){
-        for(b=1;b<=9;b++){
+        
+        for(b=1;b<=a;b++){
             printf("%d*%d=%d\t",a,b,a*b);
         }
         printf("\n");
