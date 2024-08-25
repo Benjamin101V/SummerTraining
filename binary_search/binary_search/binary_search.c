581eccfe8f6e81fb3fc6f9860a215322cac39af7
Benjamin_C <benjamin_c@qq.com>
2024-08-25 16:39:32 +0800

    Add files via upload

---
+++ binary_search/binary_search/binary_search.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k;
+	scanf("%d",&k);
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	int left = 0;
+	int right = sz - 1;
+	//int flag = 0;
+
+	while (left <= right) {
+		int mid = left+(left-right)/2;  //average value
+
+		if (arr[mid] < k) {
+			left = mid + 1;
+		}
+		else if (arr[mid] > k) {
+			right = mid - 1;
+		}
+		else {
+			printf("Get it\n");
+			printf("%d", arr[mid]);
+			//flag=1;
+			break;
+		}
+	}
+	if (left > right) {     //flag=0
+		printf("false\n");
+	}
+
+	return 0;
+}
