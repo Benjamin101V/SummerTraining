12d3388c9a62752076f363efa90186ffccd56306
Benjamin_C <benjamin_c@qq.com>
2024-09-01 16:24:49 +0800

    Add files via upload

---
+++ C/hanoi.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int count=0;
+
+int hanoi(int n,char from,char to,char with)
+{
+	
+	if(n==1){
+		count++;
+	}
+	else{
+		hanoi(n-1,from,with,to);
+		count++;
+		hanoi(n-1,with,to,from);
+	}
+	return count;
+}
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int r=hanoi(n,'a','c','b');
+	printf("%d",r);
+
+	return 0;
+}
